72f3fa457310eaa56967d7acb65e323ad0e15c43
Katarzyna Wozniak <szemkelka@gmail.com>
2021-02-18 20:02:33 +0100

    komentarze i poprawki

--- test.py
+++ test.py
@@ -1,22 +1,29 @@
 def are_anagrams(s1, s2, s3):
-    # Tutaj umieść swój kod
     l1=list(s1)
     l2=list(s2)
     l3=list(s3)
+
+#jeśli nie są tej samej długości to na pewno nie są anagramami i dalsze sprawdzanie nie ma sensu
+    if len(l1) != len(l2) or len(l2) != len(l3):
+        return False
     for i in (l1):
         if i in l2:
+ #jeśli litera z pierwszego słowa występuje w drugim to kasujemy ją z listy
             l2.remove(i)
         else:
+#jeśli w słowie drugim brakuje litery ze słowa pierwszego to nie będzie to anagram
             return False
-    if len(l2) == 0:
-        for i in l1:
-            if i in l3:
-                l3.remove(i)
-            else:
-                return False
-    if len(l3) == 0:
-        return True
-    else:
-        return False
+
+#sprawdzamy analogicznie słowo trzecie
+    for i in l1:
+        if i in l3:
+            l3.remove(i)
+        else:
+            return False
+
+#jeśli funkcja doszła do tego miejsca to znaczy że słowa są anagramami
+    return True
+
+
 
 
